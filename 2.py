c53fd289ba097d28054f912d0f95df721e43267d
Yar-Ner <48760654+Yar-Ner@users.noreply.github.com>
2022-01-18 16:15:56 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,3 +1,11 @@
-def my_func(*info):
-	print(info)
-my_func(input("Имя: "), input("Фамилия: "), input("Год рождения: "), input("Город проживания: "), input("Мыло: "), input("Телефон: "))
+def count_info():
+    try:
+        with open('2.txt', 'r', encoding="utf-8") as file:
+            my_list = file.readlines()
+            for i in range(len(my_list)):
+                new_l = my_list[i].split()
+                print(f'Количество строк в файле {len(my_list)}. В {i + 1}-ой строке {len(new_l)} слов(а)')
+    except FileNotFoundError:
+        return 'Файл не найден.'
+
+count_info()
